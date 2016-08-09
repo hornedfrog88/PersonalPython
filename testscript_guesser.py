31c11c0dec530c60d29b2df1f56c48cb8d09c1ea
Richard McGowan <Richard McGowan>
2016-08-09 13:40:34 -0700

    adding testscript_guesser

---
+++ testscript_guesser.py
@@ -0,0 +1,19 @@
+import random
+def main():
+	randomnumber = random.randint(1,1000)
+	TotNumberofGuesses = 1 
+	Found = False
+	while not Found:
+		userGuess = int(input("Guess a number between 1 and 1000: "))
+		if userGuess == randomnumber:
+			print("YOU GUESSED IT!")
+			Found = True
+		elif userGuess > randomnumber:
+			print("GUESS LOWER!")
+			TotNumberofGuesses = TotNumberofGuesses+1		
+		else:
+			print ("GUESS HIGHER!")
+			TotNumberofGuesses = TotNumberofGuesses+1
+	print ("TOTAL NUMBER OF GUESSES:",TotNumberofGuesses)		
+	print ("THANK YOU FOR PLAYING THE GUESSING GAME")
+main()
