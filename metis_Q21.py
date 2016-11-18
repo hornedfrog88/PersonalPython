3fdb2f7036c6d498b49351f70ee8cff65f3862fb
Richard McGowan <Richard McGowan>
2016-11-18 11:34:24 -0800

    add question 21 from the metis challenge'

--- metis_Q21.py
+++ metis_Q21.py
@@ -1,15 +1,61 @@
-def to_roman(number):
-    sortword1 = sorted(word1)   
-    sortword2 = sorted(word2)
-    if sortword1 == sortword2:
-        return "TRUE"
-    else:
-        return "FALSE"
+def to_roman(numeral):
+    charnumeral = str(numeral)
+    strlength = len(charnumeral)
+    onespostuple = ('I','II','III','IV','V','VI','VII','VIII','IX')
+    tenspostuple = ('X','XX','XXX','XL','L','LX','LXX','LXXX','XC')
+    hundspostuple = ('C','CC','CCC','CD','D','DC','DCC','DCCC','CM') 
+    onesroman = ''
+    tensroman = ''
+    hundsroman = ''
+    if numeral > 1000:
+        return "Number entered is out of range"
+    elif numeral == 1000:
+        return 'M'
+#Ones
+    if strlength == 1:
+        for i in range(0,9):
+            if i + 1 == numeral:
+                return onespostuple[i]
+#Tens
+    if strlength == 2:
+        numeral1 = int(charnumeral[0])
+        numeral2 = int(charnumeral[1])
+        for i in range(0,9):
+            if i + 1 == numeral1:
+                tensroman = tenspostuple[i]
+        if numeral2 > 0:
+            for k in range(0,9):
+                if k + 1 == numeral2:
+                    onesroman = onespostuple[k]
+        else:
+            onesroman = ''
+        return tensroman+onesroman
+#Hundreds
+    if strlength == 3:
+        numeral1 = int(charnumeral[0])
+        numeral2 = int(charnumeral[1])
+        numeral3 = int(charnumeral[2])      
+        for i in range(0,9):
+            if i + 1 == numeral1:
+                hundsroman = hundspostuple[i]
+        if numeral2 > 0:
+            for j in range(0,9):
+                if j + 1 == numeral2:
+                    tensroman = tenspostuple[j]
+        else:
+            tensroman = '' 
+        if numeral3 > 0:
+            for k in range(0,9):
+                if k + 1 == numeral3:
+                    onesroman = onespostuple[k]
+        else:
+            onesroman = ''
+        return hundsroman+tensroman+onesroman
+
 def main():
-    y = to_roman(15)
+    y = to_roman(1000)
     print(y)
 if __name__ == '__main__':
     main()
-
 
 
